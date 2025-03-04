5b686bae23b4a720dc1a2a05962b148dfd6a0c67
Arish Imam <arish.imam@gmail.com>
2025-03-03 21:49:06 -0800

    draw line added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from window import Window
+from window import Window, Line
 
 
 def main():
     window = Window(800, 600)
+    line1 = Line(0,0,400,400)
+    window.draw_line(line1, "black")
     window.wait_for_close()
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -25,6 +25,25 @@ class Window():
     def close(self):
         self.running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+
+        self.x2 = x2
+        self.y2 = y2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+        )
 
 
