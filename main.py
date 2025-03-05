e613ec8c1f2907c66abf8da1efded93ab07157ee
Arish Imam <arish.imam@gmail.com>
2025-03-05 07:33:40 -0800

    draw_move method added to cell class

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ from window import Window, Point, Line, Cell
 
 def main():
     window = Window(800, 600)
-    c = Cell(window)
-    #l = Line(Point(50,50), Point(400,400))
-    #window.draw_line(l, "black")
-    c.draw(50, 50, 100, 100)
-    c.draw(100, 100, 150, 150)
-    c.draw(100, 50, 150, 100)
-    c.has_right_wall = False
-    c.has_bottom_wall = False
-    c.draw(150, 50, 200, 100) 
+    c1 = Cell(window)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
+
+    c2 = Cell(window)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
+
+    c1.draw_move(c2, False)
+
+    c3 = Cell(window)
+    c3.has_right_wall = False
+    c3.has_top_wall = False
+    c3.draw(100, 100, 150, 150)
+    c2.draw_move(c3, False)
+
+
     window.wait_for_close()
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -59,7 +59,13 @@ class Cell():
         self.win = window
 
     def draw(self, x1, y1, x2, y2):
+        # if self.win == None: return???
         # draw up to 4 lines 
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
         if self.has_left_wall:
             l1 = Line(Point(x1, y1), Point(x1, y2))
             self.win.draw_line(l1, "black")
@@ -72,4 +78,27 @@ class Cell():
         if self.has_bottom_wall:
             l1 = Line(Point(x1, y2), Point(x2, y2))
             self.win.draw_line(l1, "black")
+
+    def draw_move(self, to_cell, undo=False):
+        # draw line to each cells center
+        # we likely don't have to worry about legality of the move if we only 
+        # perform this action on adjacent cells
+        # therefore lets get the center of self and the center of to_cell
+        # and simply create a line from those 2 points
+
+        self_x = self.x1 + ((self.x2 - self.x1)// 2)
+        self_y = self.y1 + ( (self.y2 - self.y1) // 2)
+        self_point = Point(self_x, self_y)
+
+
+        to_x = to_cell.x1 + ( (to_cell.x2 - to_cell.x1) // 2 )
+        to_y = to_cell.y1 + (( to_cell.y2 - to_cell.y1) // 2 )
+        to_point = Point(to_x, to_y)
+
+        l = Line(self_point, to_point)
+
+        if not undo:
+            self.win.draw_line(l, "red")
+        else:
+            self.win.draw_line(l, "gray")
 
