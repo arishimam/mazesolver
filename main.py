ab68e6614be831561a500dcb1e6a7074458d7788
Arish Imam <arish.imam@gmail.com>
2025-03-04 21:42:53 -0800

    cell drawing in progress

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from window import Window, Line
+from window import Window, Point, Line, Cell
 
 
 def main():
     window = Window(800, 600)
-    line1 = Line(0,0,400,400)
-    window.draw_line(line1, "black")
+    c = Cell(True, True, True, True, 50, 50, 100, 100, window)
+    #l = Line(Point(50,50), Point(400,400))
+    #window.draw_line(l, "black")
+    c.draw()
     window.wait_for_close()
 
 if __name__ == '__main__':
--- window.py
+++ window.py
@@ -6,11 +6,11 @@ class Window():
         self.root = Tk()
         self.root.title("Maze Solver")
 
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
 
         self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -22,11 +22,12 @@ class Window():
             self.redraw()
         print("window closed...")
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
     def close(self):
         self.running = False
 
-    def draw_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
 
 class Point():
     def __init__(self, x, y):
@@ -34,16 +35,40 @@ class Point():
         self.y = y
 
 class Line():
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = x1
-        self.y1 = y1
-
-        self.x2 = x2
-        self.y2 = y2
+    def __init__(self, point1, point2):
+        self.p1 = point1 
+        self.p2 = point2
 
     def draw(self, canvas, fill_color):
         canvas.create_line(
-            self.x1, self.y1, self.x2, self.y2, fill=fill_color, width=2
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
         )
 
+class Cell():
+    def __init__(self, left, right, top, bottom, x1, y1, x2, y2, window):
+        self.has_left_wall = left
+        self.has_right_wall = right
+        self.has_top_wall = top
+        self.has_bottom_wall = bottom 
+
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.win = window
+
+    def draw(self):
+        # draw up to 4 lines 
+        if self.has_left_wall:
+            l1 = Line(Point(self.x1, self.y1), Point(self.x1, self.y2))
+            self.win.draw_line(l1, "black")
+        if self.has_right_wall:
+            l1 = Line(Point(self.x2, self.y1), Point(self.x2, self.y2))
+            self.win.draw_line(l1, "black")
+        if self.has_top_wall:
+            l1 = Line(Point(self.x1, self.y1), Point(self.x2, self.y1))
+            self.win.draw_line(l1, "black")
+        if self.has_bottom_wall:
+            l1 = Line(Point(self.x1, self.y2), Point(self.x2, self.y2))
+            self.win.draw_line(l1, "black")
 
