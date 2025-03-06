0bf774715ed4c398b28d1a9a308cd9a256e68794
Arish Imam <arish.imam@gmail.com>
2025-03-06 07:54:47 -0800

    created maze class

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 from window import Window, Point, Line, Cell
+from maze import Maze
 
 
 def main():
     window = Window(800, 600)
-    c1 = Cell(window)
-    c1.has_right_wall = False
-    c1.draw(50, 50, 100, 100)
+    # c1 = Cell(window)
+    # c1.has_right_wall = False
+    # c1.draw(50, 50, 100, 100)
 
-    c2 = Cell(window)
-    c2.has_left_wall = False
-    c2.has_bottom_wall = False
-    c2.draw(100, 50, 150, 100)
+    # c2 = Cell(window)
+    # c2.has_left_wall = False
+    # c2.has_bottom_wall = False
+    # c2.draw(100, 50, 150, 100)
 
-    c1.draw_move(c2, False)
+    # c1.draw_move(c2, False)
 
-    c3 = Cell(window)
-    c3.has_right_wall = False
-    c3.has_top_wall = False
-    c3.draw(100, 100, 150, 150)
-    c2.draw_move(c3, False)
+    # c3 = Cell(window)
+    # c3.has_right_wall = False
+    # c3.has_top_wall = False
+    # c3.draw(100, 100, 150, 150)
+    # c2.draw_move(c3, False)
+
+    maze = Maze(50, 50, 10, 10, 50, 50, window)  
+    maze.create_cells()
 
 
     window.wait_for_close()
---
+++ maze.py
@@ -0,0 +1,55 @@
+import time
+from window import Cell 
+
+
+class Maze():
+    def __init__(
+        self,
+        x1,
+        y1, 
+        num_rows,
+        num_cols, 
+        cell_size_x,
+        cell_size_y,
+        win
+    ):
+        self.x1 = x1 
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+
+        self.cells = []
+
+    def create_cells(self):
+        for i in range(self.num_rows):
+            col = []
+            for j in range (self.num_cols):
+                c = Cell(self.win)
+                col.append(c) 
+            self.cells.append(col)
+
+
+        # call draw_cells on cells 
+        for i in range(self.num_rows):
+            for j in range (self.num_cols):
+                self.draw_cell(i,j)
+
+    def draw_cell(self, i, j):
+        x1 = self.x1 + self.cell_size_x * i
+        y1 = self.y1 + self.cell_size_y * j
+
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+
+        # how to draw each cell
+        self.cells[i][j].draw(x1, y1, x2, y2)
+        self.animate()
+
+
+
+    def animate(self):
+        self.win.redraw()
+        time.sleep(0.01)
